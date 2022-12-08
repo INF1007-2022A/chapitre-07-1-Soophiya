e068ac733bba798cbae082c1a0af3cfa73f2bb5a
Soophiya <sophie.grenier1117@gmail.com>
2022-12-08 15:02:22 -0500

    def ellipse done

--- exercice.py
+++ exercice.py
@@ -2,10 +2,23 @@
 # -*- coding: utf-8 -*-
 
 # TODO: Importez vos modules ici
-
-
+import turtle as t
+import math as m
 
 # TODO: Définissez vos fonction ici
+def ellipsoid(volume, a, b, c, density):
+
+    pi = 3.141592653589793
+
+    volume = 3/4 * pi * a * b * c
+
+    mass= density * volume
+
+    return (volume, mass) 
+
+def tree(): #draw a tree with turtle  
+
+   
 
 
 if __name__ == '__main__':
